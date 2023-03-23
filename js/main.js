f01f5633e7ec089c0897901cd86f751a322f1678
Yanez86 <91346675+Yanez86@users.noreply.github.com>
2023-03-23 21:50:24 +0100

    0.1

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ function drawGallery() {
     let imageEnd = imagesGallery - (imgForPage * (pageNeed - (pageNow + 1))) + 1;
     //disegno la gallery
     for (i = imageStart; i < imageEnd; i++) {
-        document.getElementById("gallery").innerHTML += '<a href="images/gallery/img-'+i+'.jpg"><div class="box"><img src="images/gallery/img-'+i+'.jpg"></div></a>'
+        document.getElementById("gallery").innerHTML += '<div onclick=openImg('+i+') class="box" id="myModal"><img src="images/gallery/img-'+i+'.jpg"></div>'
     }
 
     //navigator sotto la gallery
@@ -54,3 +54,28 @@ function drawGallery() {
     }
 }
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+function openImg(x){
+    console.log(x);
+    modal.style.display = "block";
+    document.getElementById("imgBig").innerHTML = '<img src="images/gallery/img-'+x+'.jpg">';
+
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
